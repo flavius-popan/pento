03bece7162c4fb5746ada664e2791407e70c3514
Flavius Popan <flaviuspopan@gmail.com>
2025-06-29 17:15:26 -0400

    guessing works

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,8 +1,16 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
+  @num_range 1..10
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       num_range: @num_range,
+       rand_int: rand_int(@num_range)
+     )}
   end
 
   def render(assigns) do
@@ -13,7 +21,7 @@ defmodule PentoWeb.WrongLive do
     </h2>
     <br />
     <h2>
-      <%= for n <- 1..10 do %>
+      <%= for n <- @num_range do %>
         <.link
           class="bg-blue-500 hover:bg-blue-700
     text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
@@ -27,18 +35,27 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
-  def handle_event("guess", %{"number" => guess}, socket) do
-    message =
-      "Your guess: #{guess}. Wrong. Guess again. "
+  def rand_int(range) do
+    Enum.random(range)
+  end
 
-    score = socket.assigns.score - 1
+  def handle_event("guess", %{"number" => guess}, socket) do
+    {message, score, rand_int} =
+      if String.to_integer(guess) != socket.assigns.rand_int do
+        {"Your guess: #{guess}. Wrong. Guess again. ", socket.assigns.score - 1,
+         socket.assigns.rand_int}
+      else
+        {"Your guess: #{guess}. Correct! Guess again. ", socket.assigns.score + 1,
+         rand_int(@num_range)}
+      end
 
     {
       :noreply,
       assign(
         socket,
         message: message,
-        score: score
+        score: score,
+        rand_int: rand_int
       )
     }
   end
