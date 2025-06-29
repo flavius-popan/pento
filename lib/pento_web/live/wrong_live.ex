8cb3a4dd1a5e575e3d17781c28a184193682263d
Flavius Popan <flaviuspopan@gmail.com>
2025-06-29 18:25:43 -0400

    extra extra credit for button colors on bad guess

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -9,7 +9,8 @@ defmodule PentoWeb.WrongLive do
       message: "Make a guess:",
       num_range: @num_range,
       secret_number: rand_int(@num_range),
-      won_game: false
+      won_game: false,
+      guesses: []
     }
   end
 
@@ -23,15 +24,20 @@ defmodule PentoWeb.WrongLive do
     <h2>
       {@message}
       <%= if @won_game do %>
-        <.link patch={~p"/guess"} class="font-bold"> Restart? </.link>
+        <.link patch={~p"/guess"} class="font-bold"> Restart</.link>
       <% end %>
     </h2>
     <br />
     <h2>
       <%= for n <- @num_range do %>
         <.link
-          class="bg-blue-500 hover:bg-blue-700
-    text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
+          class={
+            if n in @guesses,
+              do:
+                "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded m-1",
+              else:
+                "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
+          }
           phx-click="guess"
           phx-value-number={n}
         >
@@ -48,13 +54,16 @@ defmodule PentoWeb.WrongLive do
 
   def handle_event("guess", %{"number" => guess}, socket) do
     secret_number = socket.assigns.secret_number
+    guess = String.to_integer(guess)
+    guesses = socket.assigns.guesses
 
-    {message, score, secret_number, won_game?} =
-      if String.to_integer(guess) == secret_number do
-        {"Your guess: #{guess}. Correct!", socket.assigns.score + 1, rand_int(@num_range), true}
+    {message, score, secret_number, won_game?, guesses} =
+      if guess == secret_number do
+        {"Your guess: #{guess}. Correct! Guess again or", socket.assigns.score + 1,
+         rand_int(@num_range), true, []}
       else
         {"Your guess: #{guess}. Wrong. Guess again.", socket.assigns.score - 1, secret_number,
-         false}
+         false, [guess | guesses]}
       end
 
     {
@@ -64,7 +73,8 @@ defmodule PentoWeb.WrongLive do
         message: message,
         score: score,
         secret_number: secret_number,
-        won_game: won_game?
+        won_game: won_game?,
+        guesses: guesses
       )
     }
   end
