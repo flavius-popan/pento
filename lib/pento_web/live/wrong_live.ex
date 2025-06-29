c6e190da8d379efed3d40d3b83a2daa4f453cfac
Flavius Popan <flaviuspopan@gmail.com>
2025-06-29 17:54:12 -0400

    Finished extra credit

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -3,14 +3,18 @@ defmodule PentoWeb.WrongLive do
 
   @num_range 1..10
 
+  defp initial_assigns do
+    %{
+      score: 0,
+      message: "Make a guess:",
+      num_range: @num_range,
+      secret_number: rand_int(@num_range),
+      won_game: false
+    }
+  end
+
   def mount(_params, _session, socket) do
-    {:ok,
-     assign(socket,
-       score: 0,
-       message: "Make a guess:",
-       num_range: @num_range,
-       secret_number: rand_int(@num_range)
-     )}
+    {:ok, assign(socket, initial_assigns())}
   end
 
   def render(assigns) do
@@ -18,6 +22,9 @@ defmodule PentoWeb.WrongLive do
     <h1 class="mb-4 text-4xl font-extrabold">Your score: {@score}</h1>
     <h2>
       {@message}
+      <%= if @won_game do %>
+        <.link patch={~p"/guess"} class="font-bold"> Restart? </.link>
+      <% end %>
     </h2>
     <br />
     <h2>
@@ -42,12 +49,12 @@ defmodule PentoWeb.WrongLive do
   def handle_event("guess", %{"number" => guess}, socket) do
     secret_number = socket.assigns.secret_number
 
-    {message, score, secret_number} =
+    {message, score, secret_number, won_game?} =
       if String.to_integer(guess) == secret_number do
-        {"Your guess: #{guess}. Correct! Guess again. ", socket.assigns.score + 1,
-         rand_int(@num_range)}
+        {"Your guess: #{guess}. Correct!", socket.assigns.score + 1, rand_int(@num_range), true}
       else
-        {"Your guess: #{guess}. Wrong. Guess again. ", socket.assigns.score - 1, secret_number}
+        {"Your guess: #{guess}. Wrong. Guess again.", socket.assigns.score - 1, secret_number,
+         false}
       end
 
     {
@@ -56,8 +63,13 @@ defmodule PentoWeb.WrongLive do
         socket,
         message: message,
         score: score,
-        secret_number: secret_number
+        secret_number: secret_number,
+        won_game: won_game?
       )
     }
+  end
+
+  def handle_params(_unsigned_params, _uri, socket) do
+    {:noreply, assign(socket, initial_assigns())}
   end
 end
